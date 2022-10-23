c0c7396e48526266b8180bea81c7bee9159cc795
SelFerns <selwynfernandes17@gmail.com>
2022-10-23 14:27:37 +0530

    Books updated

--- src/Books.java
+++ src/Books.java
@@ -4,10 +4,10 @@ public class Books {
     HashMap<Integer,String> takenBook = new HashMap<>(); // Contains the list of books taken by the user
 
     static void initBooks(){
-        books.put(1, "Harry Potter and the Philosopher's stone");
-        books.put(2, "Fifty shades of Grey");
+        books.put(1, "Psychology of Money");
+        books.put(2, "Emotional Intelligence");
         books.put(3, "Atomic Habits");
-        books.put(4, "Everything is F*cked");
-        books.put(5, "Why Java is Shit");
+        books.put(4, "Thinking Fast and Slow");
+        books.put(5, "The 5 Second Rule");
     }
 }
